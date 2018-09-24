fa58c3e974617f42cfe9711d4e445da02c7a1a7c
WithoutLegkiyMaxim <39821754+WithoutLegkiyMaxim@users.noreply.github.com>
2018-09-24 15:41:55 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'home#less_home'
-  get '/about', to: 'home#less_about'
-  # root to: 'home#index'
-  # get '/search', to: 'home#search'
+#   root to: 'home#less_home'
+#   get '/about', to: 'home#less_about'
+  root to: 'home#index'
+  get '/search', to: 'home#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
